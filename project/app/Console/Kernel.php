e788872adde988833789b9cd6c69411af9a57dda
fw <fw@mail.ru>
2024-06-15 23:55:45 +0400

    $schedule queue:work

--- project/app/Console/Kernel.php
+++ project/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('queue:work')->everyTwoMinutes();
+        $schedule->command('queue:work')->everyMinute();
     }
 
     /**
