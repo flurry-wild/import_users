0b3d9f66e7c428def829c0f59a081c392fc546e0
fw <fw@mail.ru>
2024-06-15 20:10:14 +0400

    job

--- project/app/Jobs/ParseUsers.php
+++ project/app/Jobs/ParseUsers.php
@@ -15,51 +15,44 @@ class ParseUsers implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected UsersImportService $service;
-
-    public function setDeps(UsersImportService $service)
-    {
-        $this->service = $service;
-    }
-
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(UsersImportService $service): void
     {
-        $uploadId = $this->service->getUploadId();
+        $uploadId = $service->getUploadId();
 
         if (empty($uploadId)) {
-            $this->service->createNewUpload(1, $this->job->getJobId());
+            $service->createNewUpload(1, $this->job->getJobId());
         }
 
-        $uploadId = $this->service->getUploadId();
+        $uploadId = $service->getUploadId();
 
-        $status = $this->service->getProp($uploadId, 'status');
+        $status = $service->getProp($uploadId, 'status');
 
         if ($status === 'finished') {
-            $this->service->createNewUpload($uploadId+1, $this->job->getJobId());
+            $service->createNewUpload($uploadId+1, $this->job->getJobId());
         }
 
-        $lastProcessedRow = (int)$this->service->getProp($uploadId, 'last_processed_row');
+        $lastProcessedRow = (int)$service->getProp($uploadId, 'last_processed_row');
 
         $totalRows = Excel::toArray(new UsersImport, storage_path('app/public/import.xlsx'));
         $totalRowsCount = count($totalRows[0]);
 
         if ($lastProcessedRow + 1 >= $totalRowsCount) {
-            $this->service->setProp($uploadId, 'status', 'finished');
+            $service->setProp($uploadId, 'status', 'finished');
         } else {
             $import = new UsersImport();
             $import->startRow = $lastProcessedRow + 1;
 
             Excel::import($import, storage_path('app/public/import.xlsx'));
 
-            $this->service->setProp($uploadId, 'last_processed_row', $lastProcessedRow + 1000);
+            $service->setProp($uploadId, 'last_processed_row', $lastProcessedRow + 1000);
 
-            $jobIds = $this->service->getProp($uploadId, 'jobs');
-            $this->service->setProp($uploadId, 'jobs', implode(',', array_merge(explode(',', $jobIds), [$this->job->getJobId()])));
+            $jobIds = $service->getProp($uploadId, 'jobs');
+            $service->setProp($uploadId, 'jobs', implode(',', array_merge(explode(',', $jobIds), [$this->job->getJobId()])));
 
-            static::dispatch();
+            //static::dispatch();
         }
     }
 }
