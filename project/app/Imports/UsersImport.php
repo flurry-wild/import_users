afb2f3b42b47c70b58e4732fc34fdd99631df22e
fw <fw@mail.ru>
2024-06-17 14:33:01 +0400

    fix validation, try catch

--- project/app/Imports/UsersImport.php
+++ project/app/Imports/UsersImport.php
@@ -8,41 +8,49 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Exception;
 
 class UsersImport implements ToModel, WithHeadingRow
 {
     const LIMIT = 1000;
+    const MAX_UNSIGNED_BIG_INT = 18446744073709551615;
 
     public int $count = 0;
     public int $startRow;
 
     public function model(array $row)
     {
-        if ($this->count < static::LIMIT) {
-            $this->count++;
+        try {
+            if ($this->count < static::LIMIT) {
+                $this->count++;
 
-            $validator = Validator::make($row, [
-                'id' => 'required|int|unique:rows,external_id',
-                'name' => 'required|string|regex:/^[a-zA-Z ]+$/',
-                'date' => 'required|date|date_format:d.m.Y',
-            ]);
+                $validator = Validator::make($row, [
+                    'id' => 'required|numeric|min:0|max:' . self::MAX_UNSIGNED_BIG_INT . '|unique:rows,external_id',
+                    'name' => 'required|string|regex:/^[a-zA-Z ]+$/',
+                    'date' => 'required|date|date_format:d.m.Y',
+                ]);
 
-            if ($validator->fails()) {
-                $errors[$row['id']] = $validator->errors()->all();
-                foreach ($errors as $key => $errorMessages) {
-                    Log::channel('import_rows')->error($key . ' - ' . implode(', ', $errorMessages) . PHP_EOL);
+                if ($validator->fails()) {
+                    $errors[$row['id']] = $validator->errors()->all();
+                    foreach ($errors as $key => $errorMessages) {
+                        Log::channel('import_rows')->error($key . ' - ' . implode(', ', $errorMessages) . PHP_EOL);
+                    }
+
+                    return null;
                 }
 
-                return null;
+                return new Row([
+                    'external_id' => $row['id'],
+                    'name' => $row['name'],
+                    'date' => Carbon::createFromFormat('d.m.Y', $row['date'])->format('Y-m-d')
+                ]);
+            } else {
+                return null; // Возвращаем null, если счетчик превышен
             }
+        } catch (Exception $e) {
+            Log::error($row['id'] . ' - ' . $e->getMessage());
 
-            return new Row([
-                'external_id' => $row['id'],
-                'name' => $row['name'],
-                'date' => Carbon::createFromFormat('d.m.Y', $row['date'])->format('Y-m-d')
-            ]);
-        } else {
-            return null; // Возвращаем null, если счетчик превышен
+            return null;
         }
     }
 }
